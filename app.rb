56bf67111db8c7bbb83a6492cfb36bc335833a62
William West <wwest466@gmail.com>
2020-11-12 22:19:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     @reversed
   end
   get '/square/:number' do
-    @num = params[:number].to_i
-    @num**2.to_s
+    @num = params[:number].to_i**2
+    @num.to_s
   end
 end
